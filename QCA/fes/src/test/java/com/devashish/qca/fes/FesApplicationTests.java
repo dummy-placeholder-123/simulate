e4fdca6bcd3607da62dd0a29a93b8f6cd4d3027f
devashishrane <ranedevashish2@gmail.com>
2026-05-24 00:37:41 +0530

    Initialize Spring Boot project with Maven wrapper and basic structure

---
+++ QCA/fes/src/test/java/com/devashish/qca/fes/FesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devashish.qca.fes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
